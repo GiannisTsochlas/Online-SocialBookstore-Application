9c9a28015417376c11ef3de4c040af761fba3e6b
KLamaniakou <kostaslamaniakos62@gmail.com>
2024-05-06 22:44:57 +0300

    start

--- src/main/java/com/example/SocialBookstore/SocialBookstoreApplication.java
+++ src/main/java/com/example/SocialBookstore/SocialBookstoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SocialBookstoreApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SocialBookstoreApplication.class, args);
 	}
 
