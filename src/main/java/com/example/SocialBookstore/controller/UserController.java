60ae14b4a6378abcf9fe48a1574b053123222556
KLamaniakou <kostaslamaniakos62@gmail.com>
2024-05-07 20:28:23 +0300

    Mexri US4

--- src/main/java/com/example/SocialBookstore/controller/UserController.java
+++ src/main/java/com/example/SocialBookstore/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-
+    private boolean flag = false;
     @RequestMapping("/user/dashboard")
     public String getUserHome(){
 		
@@ -13,10 +13,14 @@ public class UserController {
     }
     @RequestMapping("/profile")
     public  String Createprofile(){
+        flag=true;
         return "user/profile";
     }
     @RequestMapping("/submit_profile")
     public  String Subprofile(){
+        if (!flag){
+            return "user/submit_profile_null";
+        }
         return "user/submit_profile";
     }
 }
